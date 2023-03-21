c30a18905d533f565d41fb65c8376056b4134cc8
ygbr5060 <aqibkhan5060@gmail.com>
2023-03-21 23:21:09 +0000

    updated js script code

--- script.js
+++ script.js
@@ -90,17 +90,66 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var length = parseInt(prompt("Enter length of password (between 8 and 128):"));
+   // Validate password length
+   if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid password length! Password length must be between 8 and 128.");
+    return null;
+  }
 
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters ($@%&*, etc.)?");
+
+  // Validate at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("Invalid character types! Please select at least one character type.");
+    return null;
+  }
+
+  // Return an object with password options
+  return {
+    length: length,
+    includeLowercase: includeLowercase,
+    includeUppercase: includeUppercase,
+    includeNumeric: includeNumeric,
+    includeSpecial: includeSpecial
+  };
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
 }
 
 // Function to generate password with user input
-function generatePassword() {
-
+function generatePassword()  {
+    var options = getPasswordOptions();
+  
+    if (!options) {
+      // If no options, return an empty string
+      return "";
+    }
+  
+    var password = "";
+    var possibleChars = [];
+  
+    if (options.includeLowercase) {
+      possibleChars = possibleChars.concat(lowerCasedCharacters);
+      password += getRandom(lowerCasedCharacters);
+    }
+  
+    if (options.includeUppercase) {
+      possibleChars = possibleChars.concat(upperCasedCharacters);
+      password += getRandom(upperCasedCharacters);
+    }
+  
+    if (options.includeNumeric) {
+      possibleChars = possibleChars.concat(numericCharacters);
+      password += getRandom(numericCharacters);
+    }
 }
 
 // Get references to the #generate element
